edbfeb939f70c06a91f4ac632b2f68f353f29d1d
Ali Reda <alyredagomaa@gmail.com>
2025-02-22 14:04:44 +0200

    update content

--- src/pages/basics/styling/Design.jsx
+++ src/pages/basics/styling/Design.jsx
@@ -6,19 +6,73 @@ let Design = () => (
       I only use design as a tool in web development
       ************************************************************************************************
       # basics & common 
-
-        * UI/UX design tools => figma - adobexd - sketch
+      
+      * UI/UX design tools => figma - adobexd - sketch
+      
+      ************************************************************************************************
       
       # figma
         * UI/UX process steps :
           1- making docs for brain storm ideas & tech. requirements 
             $ [ UX ] [ priority to real user { people interview & survey } ] 
           2- sketching & designing  
-      
-        * tips : 
+        
+        *  info :
+          - multiple components ( reusable instances “parts” ) [ switch between its items ] - detach -   instance
+          - properties ( instant options ) vs variants ( instatnt type )
+          - grouping & sections & frames 
+          - draw illustration using pen 
+          - shapes intersection 
+          - image mask => equals frames in canva
+          - site map : hierarchical diagrams like tree show navigation in pages
+          - color palette => colors you will use in your project
+          - figma pages => app page - assets page (components)
+          - onboarding page { first page on opening app }
+          - design patterns => solutions for common design problems
+          - user flow => wire frame => prototype 
+            $ user flow : explain features like registration process or searching process
+            $ wire frames : pages content like ( home , cart , account ) pages     
+            $ prototype ( show in mockups) ( declare [user actions ] Ex. hover )
+
+
+        * plugins 
+          -  images => ( unsplash - freepik ) 
+          -  icons => ( material - iconify - streamline ) 
+          - illustration graphs => ( blush - illustrations - streamline )   
+          - content real to fill content  
+          - LottieFiles for animated gifs   
+          - remove bg 
+
+          
+        * tips & best practices : 
+          - use grid & rulers 
           - take ideas from competitors & well designed websites & templates 
+          - explain details ( not be lazy )
+          -  ( variation ) using many color & sizes & fonts in one part
+          - get a continuous feedback team & client  
+          - many design options to choose one from them 
+          - inputs and interactive feedback ( invalid - valid )
+          - use motions => by modify opacity of frame items ( opacity - position - size - rotate - color - effects & filterss ) 
+          - use responsive
+            $ responsive  ====>>> auto layout & fill container  
+            $ constrains ( auto responsive width and height ) 
+          - use centralized control & reusability  
+            $ local styles Ex. primary color 
+            $ variants
+            $ components
+          - consider all people cases 
+            $ accessability & difficulties 
+            $ children & ages 
+            $ different background & education & jobs
+            $ tech knowledge level    
+          - use figma community for => 
+            $ swm icons 
+            $ pre-made components & widgets
+          
 
-
+          
+      ************************************************************************************************
+      
       # Canva : 
         * girl photo cropped in circle Frame ( graphic or shape ) or Mockup ( real life item 
           - Ex. t-shirt or mobile  )  or Grid to set a photo background
--- src/pages/database/Common.jsx
+++ src/pages/database/Common.jsx
@@ -1,29 +1,28 @@
 export default function Common() {
   return (
     <pre>
-      ### Basics Info
-      <span>
-        ** you can download SQL server on you machine OR get it as software service like XXAMP , LARAGON
-        ** we make a connect between app & server ALSO we call server before every query
-        ** ORM Object Relational Mapping
-      </span>
+      {`
+      # Basics Info
+        * you can download SQL server on you machine OR get it as software service like XXAMP , LARAGON
+        * we make a connect between app & server ALSO we call server before every query
+        * ORM Object Relational Mapping
 
-      ### difference between SQL & NSQL
-      <span>
-        ** the difference is structure & schema & content validation PLUS relations
-        ** if you not follow structure in SQL you get error unlike NSQL that is is schema-less
-        ** NSQL has no or low relations unlike SQL
-        ** NSQL duplicate data
-        ** NSQL more size but best performance
-        ** SQL uses many table |||| but NSQL merges data in a few collections
-        ** SQL server has limits in read & write per second but NSQL has massive performance
-      </span>
+      # difference between SQL & NoSQL
+        * the difference is structure & schema & content validation PLUS relations
+        * if you not follow structure in SQL you get error unlike NoSQL that is is schema-less
+        * NoSQL has no or low relations unlike SQL
+        * NoSQL duplicate data
+        * NoSQL more size but best performance
+        * SQL uses many table |||| but NoSQL merges data in a few collections
+        * SQL => records in tables  ||||||| noSQL => documents in collections 
+        * SQL server has limits in read & write per second but NoSQL has massive performance
 
-      ### database scaling
-      <span>
-        ** vertical : more powerful one server  ( SQL & NSQL )
-        ** horizontal : many low powerful servers ( NSQL Only )
-      </span>
+      # database scaling
+        * vertical : more powerful one server  ( SQL & NoSQL )
+        * horizontal : many low powerful servers ( NoSQL Only )
+      
+      `}
+
     </pre>
   );
 }
