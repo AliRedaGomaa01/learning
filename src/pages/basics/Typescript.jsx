671a88d1f1048b97c1f9f86ba49c0decf630d6a5
Ali Reda <alyredagomaa@gmail.com>
2025-02-22 11:14:01 +0200

    update content

--- src/pages/basics/Typescript.jsx
+++ src/pages/basics/Typescript.jsx
@@ -2,9 +2,10 @@ export default function Typescript() {
   return (
     <>
       {`
-      # it's built on js and is compiled to js to be used on browser
-      # it prevents unexpected behavior & unexpected errors 
-      # in typescript we can implement interfaces
+      # TS  built on js (dynamically types) and is compiled to js to be used on browser
+      # it prevents unexpected behavior & unexpected errors
+      # it gives errors only in type-script-file when coding
+      # it's compiled to normal js without type errors
 
       # commands : 
         * npm install typescript ts-node  // install typescript & ts-node globally
@@ -13,10 +14,13 @@ export default function Typescript() {
         * tsc // compile all ts files
       
       # define types
+        * type inference => it inits the type dynamically for simple type ex. => let myVar = 'ali';
+        * interface MyCustomType2 { key1 : string ; key2 : string[] ; }   // can be implemented by classes
         * type MyCustomType = string | string[]  
           const myVar : MyCustomType = "myVal";
-        * interface MyCustomType2 { key1 : string ; key2 : string[] ; }  
         * casting => "1" as number 
+        * it defines class attributes types with privatable Ex.
+          - class MyClass { attr1 : string ; private attr2 : number ; constructor ( public attr3: string  ) { } }   
         * myFnCall<{ name!: 'Ali'|'Ahmed'|'unionType' , age?: number }[] | undefined>( myArr )  
           - parameter variable type validation on call 
         * myFnDef( par1: string , par2 : any ) : returnType { fnBody } 
@@ -25,11 +29,13 @@ export default function Typescript() {
           - gets the type of this object that will be assigned later to this variable
         * @ClassType{  } Class ClassName {}
         * generic ( general not specific ) 
-          - accept more than one of any type like array , function params
+          - accept more than one of any type like array , function params when calling
+          - its items has any as a default type
           - used with complex types 
           - Ex
               let myArr : Array<string | number> = [];
-              function identity<T>(arg: T): T { return arg; }
+              function identity<T>(arg: T): T { return arg; } 
+                // means all arg & params & return should be one type
               console.log(identity<number>(10));  // Output: 10
               console.log(identity<string>("Hello")); 
 
