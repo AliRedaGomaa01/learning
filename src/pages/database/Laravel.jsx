8aa2df5e51ad08aa8441c1f7cbba03dd9be76616
Ali Reda <alyredagomaa@gmail.com>
2025-03-25 17:32:11 +0200

    update content

--- src/pages/database/Laravel.jsx
+++ src/pages/database/Laravel.jsx
@@ -2,7 +2,8 @@ export default function Laravel() {
   return (
     <pre>
       {`
-
+        # important info 
+          * we can use mongo DB with laravel by using 3rd parties 
       `}
     </pre>
   );
