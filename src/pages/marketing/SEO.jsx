318acdc2a22f8ca17e8564db5545012d50a2d6f2
Ali Reda <alyredagomaa@gmail.com>
2025-04-12 21:38:27 +0200

    update content & rebuild & add htaccess

--- src/pages/marketing/SEO.jsx
+++ src/pages/marketing/SEO.jsx
@@ -1,8 +1,6 @@
 let SEO = () => (
   <pre>
 {`
-* we should use unique meta tags & title for each path
-* h1 to h6 is used to index the structure of the page
 * use server side rendering 
   - inertia => semi server side rendering 
 * use all possible meta tags 
@@ -11,13 +9,30 @@ let SEO = () => (
 * use SEO measurement tools
 * using marketing strategies is 500% important than using SEO 
 * redirecting is not good for SEO
+* the website should be fast in speed test ( < 3 seconds ) 
+  - gtmetrix.com to test speed 
 
 * not depends only on the code but also on the next tips
   - brand name repetition & uniqueness
   - clear chosen domain 
   - frequent searching from users in Search Engines
   - How long  was it deployed  from ? ( months - years - ... )
-      
+
+* on page SEO :
+  - keywords : ads.google.com ( by keyword , by website )  , mozBar chrome extension 
+  - we should use unique meta tags & title for each path
+  - h1 to h6 is used to index the structure of the page
+  - repeat most used keywords in the content
+  - content punctuation & grammar is important
+  - formatting content to be eye friendly &  responsive
+  - follow best practices like => image alt text , close tags 
+  - keyword & domain & title => short without repeat 
+  - Yoast SEO for wordpress to give feedback for each page
+  - use internal & external links in your articles 
+  - optimize images & compress its size (imagecompressor.com) 
+
+* marketing tools : 
+  - email : mailchimp
 ` }
   </pre>
 
