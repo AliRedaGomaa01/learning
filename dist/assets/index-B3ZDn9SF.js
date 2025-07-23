52979cbd320ab593d830de75fe27062a9d27daa8
Ali Reda <alyredagomaa@gmail.com>
2025-07-23 18:28:56 +0300

    update content & rebuild & add htaccess

--- dist/assets/index-B3ZDn9SF.js
+++ dist/assets/index-B3ZDn9SF.js
@@ -2209,15 +2209,10 @@ I only use design as a tool in web development
 - redirecting
 $ to file in spa apache react
 <IfModule mod_rewrite.c>
-  RewriteEngine On
-  RewriteBase /
-
-  # Only rewrite if the request is NOT an existing file or directory
-  RewriteCond %{REQUEST_FILENAME} !-f
-  RewriteCond %{REQUEST_FILENAME} !-d
-
-  # Redirect all other requests to index.html inside dist/
-  RewriteRule ^(.*)$ /dist/index.html [L]
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} -f [OR]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteRule ^(.*)$ dist/index.html [L,QSA]
 </IfModule>
 
 $ to file 
